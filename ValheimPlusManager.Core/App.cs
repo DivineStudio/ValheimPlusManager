f7a5736a64f5bfa324cbe29ad17202ff9bd7e4b5
Dalton <ponder.dalton.ph@gmail.com>
2021-03-07 23:29:57 -0600

    Finish initial implementation apps mvvm structure

---
+++ ValheimPlusManager.Core/App.cs
@@ -0,0 +1,19 @@
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using ValheimPlusManager.Core.ViewModels;
+
+namespace ValheimPlusManager.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<ApplicationViewModel>();
+        }
+    }
+}
---
+++ ValheimPlusManager.UI/Views/ApplicationView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MvvmCross.Platforms.Wpf.Views;
+using MvvmCross.ViewModels;
+using ValheimPlusManager.Core.ViewModels;
+
+namespace ValheimPlusManager.UI.Views
+{
+    [MvxViewFor(typeof(ApplicationViewModel))]
+    public partial class ApplicationView : MvxWpfView
+    {
+        public ApplicationView()
+        {
+            InitializeComponent();
+        }
+    }
+}
